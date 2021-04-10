fd4fcc169e0bab557c8188d7703324e6280075cf
Ender Wang <33310255+4A9EBB44@users.noreply.github.com>
2021-04-10 22:57:31 +0800

    v0.2 of sensor data collection
    standardized data output stream, to communicate with ESP8266.

---
+++ humidity_temperature_waterLevel/humidity_water_level.ino
@@ -0,0 +1,54 @@
+//humidity class
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+
+//water level
+int resval = 0;  // holds the value
+int respin = A5; // sensor pin used
+
+
+void setup() {
+  // start the serial console
+  Serial.begin(9600);
+}
+
+void loop() {
+  //loop for humidity sensor
+  // start working...
+  Serial.println("=================");
+  delay(1000);
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+  Serial.print("T");//T:int;
+  Serial.print((float)temperature); Serial.println(";");
+  //make the delay consistant to humidity sensor
+  delay(1000);
+  Serial.print("H"); //H:int;
+  Serial.print((int)humidity); Serial.println(";");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1000);
+
+  //loop for water level sensor
+   resval = analogRead(respin); //Read data from analog pin and store it to resval variable
+  if (resval<=100){ Serial.println("WE;"); }//W:E/L/M/H;
+  else if (resval>100 && resval<=600){ Serial.println("WL;"); }
+  else if (resval>600 && resval<=660){ Serial.println("WM;"); } 
+  else if (resval>660){ 
+    Serial.println("WH;"); 
+  }
+  delay(1000); 
+}
